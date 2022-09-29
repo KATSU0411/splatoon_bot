0f4e5c34d60c9474b03e9306e7595ac6e5bb1249
KATSU0411 <taka.katsu411@gmail.com>
2022-09-29 11:48:47 +0900

    ウィンドウ位置仮取得

--- src/gui/main_gui.py
+++ src/gui/main_gui.py
@@ -1,10 +1,32 @@
 import PySimpleGUI as SG
+from ahk import AHK
+
+import ctypes
+from ctypes.wintypes import HWND, DWORD, RECT
+
+# window位置とサイズ取得
+# todo: クラス化
+
+def getWindowRect(window_title):
+    target_window_handle = ctypes.windll.user32.FindWindowW(0, window_title)
+    rect = ctypes.wintypes.RECT()
+    ctypes.windll.user32.GetWindowRect(
+            target_window_handle, ctypes.pointer(rect))
+    print((rect.left, rect.top, rect.right, rect.bottom))
+    return (rect.left, rect.top, rect.right, rect.bottom)
+
+
 
 SG.theme('LightBlue1')
 
-layout = [  [SG.Text('line text')],
-        [SG.Text('input'), SG.InputText()],
-        [SG.Button('OK'), SG.Button('Cancel')] ]
+ahk = AHK()
+window_infos = list(ahk.windows())
+window_titles = [info.title.decode('utf-8', errors='ignore') for info in window_infos]
+
+layout = [  
+            [SG.Text('line text')],
+            [SG.Combo(window_titles, default_value='キャプチャする対象を選択', key='WINDOW_TITLE', enable_events=True)]
+        ]
 
 window = SG.Window(title='splatoon auto move voice chat tool', layout=layout)
 
@@ -12,5 +34,12 @@ while True:
     event, values = window.read()
     if event == SG.WIN_CLOSED:
         break
+
+    if event == 'WINDOW_TITLE':
+        title = values['WINDOW_TITLE']
+        print(title)
+
+        getWindowRect(title)
+
 
 window.close
